ca9d193f8384edbfdb70b9a30b078085777f691e
ankuraxz <34418414+Ankuraxz@users.noreply.github.com>
2024-09-29 02:40:56 -0400

    icon location change

--- src/components/common/FloatingChatIcon.js
+++ src/components/common/FloatingChatIcon.js
@@ -5,14 +5,13 @@ import { Link } from 'react-router-dom';
 
 const FloatingChatIcon = () => {
   return (
-    <Link
-      to="/chat"
-      className="fixed bottom-5 right-5 bg-blue-500 text-white rounded-full p-3 shadow-lg hover:bg-blue-600 transition duration-300 animate-bounce"
-    >
-      <IoChatbubbles className="text-2xl" />
-    </Link>
+      <Link
+          to="/chat"
+          className="fixed bottom-20 right-36 bg-blue-500 text-white rounded-full p-3 shadow-lg hover:bg-blue-600 transition duration-300 animate-bounce"
+      >
+        <IoChatbubbles className="text-5xl" />
+      </Link>
   );
 };
 
 export default FloatingChatIcon;
-
